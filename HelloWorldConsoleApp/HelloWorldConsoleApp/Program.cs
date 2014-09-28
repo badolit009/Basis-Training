1fdc9256a9f6458adc01d4b8e0617e8fb88620a3
Badol Hossain <badolit009@gmail.com>
2014-09-27 23:18:02 -0700

    Hello world Added

---
+++ HelloWorldConsoleApp/HelloWorldConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; 
+
+namespace HelloWOrldConsoleApps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Hello World");
+            
+            int number=64;
+            char character=Convert.ToChar(number);
+            Console.WriteLine(character);
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ HelloWorldDesktopApps/HelloWorldDesktopApps/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace HelloWorldDesktopApps
+{
+    partial class HelloWorld
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.showButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(96, 79);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 0;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // HelloWorld
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.showButton);
+            this.Name = "HelloWorld";
+            this.Text = "Hello World Apps";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button showButton;
+    }
+}
+
