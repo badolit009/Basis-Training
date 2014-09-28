d1707a3a2bc7a3dbe7e0a7d5913deffb8c2c78fe
Badol Hossain <badolit009@gmail.com>
2014-09-27 23:21:52 -0700

    BankingPercentApp Added

---
+++ BankingPercent/BankingPercent/BankingPercent.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace BankingPercent
+{
+    partial class BankingPercent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.TimeTextBox = new System.Windows.Forms.TextBox();
+            this.BlanceTextBox = new System.Windows.Forms.TextBox();
+            this.calculateInterestButton = new System.Windows.Forms.Button();
+            this.interestlabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(87, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bank Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Time(inYear)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(95, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Balance";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "BRAK",
+            "DBBL",
+            "HSBC"});
+            this.comboBox1.Location = new System.Drawing.Point(156, 46);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // TimeTextBox
+            // 
+            this.TimeTextBox.Location = new System.Drawing.Point(156, 88);
+            this.TimeTextBox.Name = "TimeTextBox";
+            this.TimeTextBox.Size = new System.Drawing.Size(121, 20);
+            this.TimeTextBox.TabIndex = 4;
+            // 
+            // BlanceTextBox
+            // 
+            this.BlanceTextBox.Location = new System.Drawing.Point(156, 136);
+            this.BlanceTextBox.Name = "BlanceTextBox";
+            this.BlanceTextBox.Size = new System.Drawing.Size(121, 20);
+            this.BlanceTextBox.TabIndex = 5;
+            // 
+            // calculateInterestButton
+            // 
+            this.calculateInterestButton.Location = new System.Drawing.Point(98, 185);
+            this.calculateInterestButton.Name = "calculateInterestButton";
+            this.calculateInterestButton.Size = new System.Drawing.Size(205, 23);
+            this.calculateInterestButton.TabIndex = 6;
+            this.calculateInterestButton.Text = "Calculate Interest";
+            this.calculateInterestButton.UseVisualStyleBackColor = true;
+            this.calculateInterestButton.Click += new System.EventHandler(this.calculateInterestButton_Click);
+            // 
+            // interestlabel
+            // 
+            this.interestlabel.AutoSize = true;
+            this.interestlabel.Location = new System.Drawing.Point(95, 236);
+            this.interestlabel.Name = "interestlabel";
+            this.interestlabel.Size = new System.Drawing.Size(48, 13);
+            this.interestlabel.TabIndex = 7;
+            this.interestlabel.Text = "Interest=";
+            // 
+            // BankingPercent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 303);
+            this.Controls.Add(this.interestlabel);
+            this.Controls.Add(this.calculateInterestButton);
+            this.Controls.Add(this.BlanceTextBox);
+            this.Controls.Add(this.TimeTextBox);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "BankingPercent";
+            this.Text = "BankingPercent";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox TimeTextBox;
+        private System.Windows.Forms.TextBox BlanceTextBox;
+        private System.Windows.Forms.Button calculateInterestButton;
+        private System.Windows.Forms.Label interestlabel;
+    }
+}
+
---
+++ BankingPercent/BankingPercent/BankingPercent.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BankingPercent
+{
+    public partial class BankingPercent : Form
+    {
+        public BankingPercent()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateInterestButton_Click(object sender, EventArgs e)
+        {
+            double balance, year, interest;
+            year = Convert.ToDouble(TimeTextBox.Text);
+            balance  = Convert.ToDouble(BlanceTextBox.Text);
+            if (comboBox1.Text == "BRAK")
+            {
+                interest = year*(balance*0.06);
+                interestlabel.Text = interest.ToString();
+            }
+            else if (comboBox1.Text == "DBBL")
+            {
+                interest = year * (balance * 0.07);
+                interestlabel.Text = interest.ToString();
+                
+            }
+            else if (comboBox1.Text == "HSBC")
+            {
+                interest = year * (balance * 0.08);
+                interestlabel.Text = interest.ToString();
+            }
+            
+                
+
+        
+        }
+    }
+}
