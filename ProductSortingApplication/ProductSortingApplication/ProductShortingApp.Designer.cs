3f6af46e0e79ebe13c18c62bb033893b4ccbbbe4
Badol Hossain <badolit009@gmail.com>
2014-09-28 23:35:09 -0700

    ProductShorting Uploaded Successfully

---
+++ ProductSortingApplication/ProductSortingApplication/ProductShortingApp.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace ProductSortingApplication
+{
+    partial class ProductShortingApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.selectComboBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.itemTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.addTextBox = new System.Windows.Forms.Button();
+            this.clearTextBox = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select Item";
+            // 
+            // selectComboBox
+            // 
+            this.selectComboBox.FormattingEnabled = true;
+            this.selectComboBox.Items.AddRange(new object[] {
+            "Asus",
+            "Dell",
+            "HP",
+            "Lenevo",
+            "Toshiba",
+            "Samsung"});
+            this.selectComboBox.Location = new System.Drawing.Point(106, 38);
+            this.selectComboBox.Name = "selectComboBox";
+            this.selectComboBox.Size = new System.Drawing.Size(165, 21);
+            this.selectComboBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(277, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Number Of Item";
+            // 
+            // itemTextBox
+            // 
+            this.itemTextBox.Location = new System.Drawing.Point(382, 37);
+            this.itemTextBox.Name = "itemTextBox";
+            this.itemTextBox.Size = new System.Drawing.Size(178, 20);
+            this.itemTextBox.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Current Total Amount:";
+            // 
+            // addTextBox
+            // 
+            this.addTextBox.Location = new System.Drawing.Point(458, 64);
+            this.addTextBox.Name = "addTextBox";
+            this.addTextBox.Size = new System.Drawing.Size(101, 21);
+            this.addTextBox.TabIndex = 5;
+            this.addTextBox.Text = "Add";
+            this.addTextBox.UseVisualStyleBackColor = true;
+            this.addTextBox.Click += new System.EventHandler(this.addTextBox_Click);
+            // 
+            // clearTextBox
+            // 
+            this.clearTextBox.Location = new System.Drawing.Point(459, 91);
+            this.clearTextBox.Name = "clearTextBox";
+            this.clearTextBox.Size = new System.Drawing.Size(101, 21);
+            this.clearTextBox.TabIndex = 6;
+            this.clearTextBox.Text = "Clear";
+            this.clearTextBox.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.Location = new System.Drawing.Point(42, 131);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(516, 137);
+            this.listView1.TabIndex = 7;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Serial No";
+            this.columnHeader1.Width = 95;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Product Name";
+            this.columnHeader2.Width = 147;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Unit Price";
+            this.columnHeader3.Width = 173;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Total Price";
+            this.columnHeader4.Width = 80;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(167, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 8;
+            // 
+            // ProductShortingApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 278);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.clearTextBox);
+            this.Controls.Add(this.addTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.itemTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.selectComboBox);
+            this.Controls.Add(this.label1);
+            this.Name = "ProductShortingApp";
+            this.Text = "ProductShortingApp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox selectComboBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox itemTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button addTextBox;
+        private System.Windows.Forms.Button clearTextBox;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ProductSortingApplication/ProductSortingApplication/ProductShortingApp.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProductSortingApplication
+{
+    public partial class ProductShortingApp : Form
+    {
+        public ProductShortingApp()
+        {
+            InitializeComponent();
+        }
+        int i = 0;
+        private void addTextBox_Click(object sender, EventArgs e)
+        {
+            string selectItem = selectComboBox.Text;
+            double unitPrice = 0;
+
+            switch (selectItem)
+            {
+                case "Asus":
+                unitPrice=8000;
+                break;
+                    
+                case "Dell":
+                    unitPrice=8500;
+                    break;
+
+                case "HP":
+                    unitPrice=9000;
+                    break;
+
+                case "Toshiba":
+                    unitPrice=7000;
+                    break;
+
+            }
+
+            int itemNumber=Convert.ToInt32( itemTextBox.Text);
+            double totalPrice = unitPrice * itemNumber;
+            label4.Text = Convert.ToString(totalPrice)+"/=";
+            int counter = ++i;
+            ListViewItem liv = new ListViewItem(counter.ToString());
+            liv.SubItems.Add(selectItem);
+            liv.SubItems.Add(unitPrice.ToString());
+            liv.SubItems.Add(totalPrice.ToString());
+            listView1.Items.Add(liv);
+            
+        }
+
+    }
+}
