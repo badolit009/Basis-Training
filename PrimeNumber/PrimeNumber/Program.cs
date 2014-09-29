07918a9828437adba8d568df609adb4b92a5fe62
Badol Hossain <badolit009@gmail.com>
2014-09-28 23:33:24 -0700

    primeNumber get 1 to 100 Added Successfully

---
+++ PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int i, j;
+            Console.WriteLine("prime no between 1 to 100");
+            for (i = 1; i <= 100; i++)
+            {
+                int count = 0;
+                for (j = 1; j <= i; j++)
+                {
+
+                    if (i % j == 0)
+                    { count = count + 1; }
+                }
+
+                if (count <= 2)
+                { Console.WriteLine(i); }
+
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
