eae8ee3f11146f2a1177131c9028f0e33dbdb42b
Badol Hossain <badolit009@gmail.com>
2014-09-28 18:10:43 -0700

    Initial FindingArrayIndex Uploaded

---
+++ FindingArrayIndex/FindingArrayIndex/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FindingArrayIndex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string Name = "Hello null World";
+
+            int start = 0, lPosition = 0, oPosition = 0;
+
+            Console.Write("'L' Position is : ");
+
+            while (start < Name.Length)
+            {
+
+                lPosition = Name.IndexOf('l', start);
+                if (lPosition == -1)
+                    break;
+                start = lPosition + 1;
+                Console.Write(lPosition + " ");
+            }
+        }
+    }
+}
