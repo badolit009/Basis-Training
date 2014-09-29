3d4dd7264a3e8a7154517c93e0a290073e2a9b59
Badol Hossain <badolit009@gmail.com>
2014-09-28 23:44:19 -0700

    CustomerQueueManagement without user define type Added successfully

---
+++ CustomerQueueManagement/CustomerQueueManagement/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace CustomerQueueManagement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.eNameTextBox = new System.Windows.Forms.TextBox();
+            this.enqueueButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.eComplainTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dComplainTextBox = new System.Windows.Forms.TextBox();
+            this.dNameTextBox = new System.Windows.Forms.TextBox();
+            this.dSerialNoTextBox = new System.Windows.Forms.TextBox();
+            this.dequeueButton = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.serialNo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.complain = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name :";
+            // 
+            // eNameTextBox
+            // 
+            this.eNameTextBox.Location = new System.Drawing.Point(99, 27);
+            this.eNameTextBox.Name = "eNameTextBox";
+            this.eNameTextBox.Size = new System.Drawing.Size(191, 20);
+            this.eNameTextBox.TabIndex = 1;
+            // 
+            // enqueueButton
+            // 
+            this.enqueueButton.Location = new System.Drawing.Point(279, 84);
+            this.enqueueButton.Name = "enqueueButton";
+            this.enqueueButton.Size = new System.Drawing.Size(125, 33);
+            this.enqueueButton.TabIndex = 2;
+            this.enqueueButton.Text = "Enqueue";
+            this.enqueueButton.UseVisualStyleBackColor = true;
+            this.enqueueButton.Click += new System.EventHandler(this.enqueueButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.eComplainTextBox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.enqueueButton);
+            this.groupBox1.Controls.Add(this.eNameTextBox);
+            this.groupBox1.Location = new System.Drawing.Point(50, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(410, 123);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Enqueue Customer";
+            // 
+            // eComplainTextBox
+            // 
+            this.eComplainTextBox.Location = new System.Drawing.Point(99, 58);
+            this.eComplainTextBox.Name = "eComplainTextBox";
+            this.eComplainTextBox.Size = new System.Drawing.Size(305, 20);
+            this.eComplainTextBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Complain :";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.dComplainTextBox);
+            this.groupBox2.Controls.Add(this.dNameTextBox);
+            this.groupBox2.Controls.Add(this.dSerialNoTextBox);
+            this.groupBox2.Controls.Add(this.dequeueButton);
+            this.groupBox2.Location = new System.Drawing.Point(489, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(372, 123);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Dequeue Customer";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 90);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Complain :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Name :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Serial No :";
+            // 
+            // dComplainTextBox
+            // 
+            this.dComplainTextBox.Location = new System.Drawing.Point(89, 85);
+            this.dComplainTextBox.Name = "dComplainTextBox";
+            this.dComplainTextBox.Size = new System.Drawing.Size(260, 20);
+            this.dComplainTextBox.TabIndex = 6;
+            // 
+            // dNameTextBox
+            // 
+            this.dNameTextBox.Location = new System.Drawing.Point(89, 59);
+            this.dNameTextBox.Name = "dNameTextBox";
+            this.dNameTextBox.Size = new System.Drawing.Size(171, 20);
+            this.dNameTextBox.TabIndex = 5;
+            // 
+            // dSerialNoTextBox
+            // 
+            this.dSerialNoTextBox.Location = new System.Drawing.Point(89, 33);
+            this.dSerialNoTextBox.Name = "dSerialNoTextBox";
+            this.dSerialNoTextBox.Size = new System.Drawing.Size(171, 20);
+            this.dSerialNoTextBox.TabIndex = 4;
+            // 
+            // dequeueButton
+            // 
+            this.dequeueButton.Location = new System.Drawing.Point(266, 23);
+            this.dequeueButton.Name = "dequeueButton";
+            this.dequeueButton.Size = new System.Drawing.Size(100, 33);
+            this.dequeueButton.TabIndex = 4;
+            this.dequeueButton.Text = "Dequeue";
+            this.dequeueButton.UseVisualStyleBackColor = true;
+            this.dequeueButton.Click += new System.EventHandler(this.dequeueButton_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.listView1);
+            this.groupBox3.Location = new System.Drawing.Point(50, 159);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(811, 239);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Waiting Queue";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.serialNo,
+            this.name,
+            this.complain});
+            this.listView1.Location = new System.Drawing.Point(9, 19);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(796, 199);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // serialNo
+            // 
+            this.serialNo.Text = "Serial No:";
+            // 
+            // name
+            // 
+            this.name.Text = "Name:";
+            // 
+            // complain
+            // 
+            this.complain.Text = "Complain";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 410);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox eNameTextBox;
+        private System.Windows.Forms.Button enqueueButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox eComplainTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox dComplainTextBox;
+        private System.Windows.Forms.TextBox dNameTextBox;
+        private System.Windows.Forms.TextBox dSerialNoTextBox;
+        private System.Windows.Forms.Button dequeueButton;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader serialNo;
+        private System.Windows.Forms.ColumnHeader name;
+        private System.Windows.Forms.ColumnHeader complain;
+    }
+}
+
---
+++ CustomerQueueManagement/CustomerQueueManagement/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CustomerQueueManagement
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int count = 0;
+        Queue<string> aQueue = new Queue<string>();
+        
+
+        private void enqueueButton_Click(object sender, EventArgs e)
+        {
+            string serial = Convert.ToString(++count);
+            string name = eNameTextBox.Text;
+            string complain = eComplainTextBox.Text;
+           
+            aQueue.Enqueue(serial);
+            aQueue.Enqueue(name);
+            aQueue.Enqueue(complain);
+
+            eNameTextBox.Text = "";
+            eComplainTextBox.Text = "";
+
+            ListViewItem liv = new ListViewItem(serial);
+            liv.SubItems.Add(name);
+            liv.SubItems.Add(complain);
+            listView1.Items.Add(liv);
+
+
+        }
+
+        private void dequeueButton_Click(object sender, EventArgs e)
+        {
+            if (listView1.Items.Count != 0)
+            {
+                dSerialNoTextBox.Text = aQueue.Dequeue();
+                dNameTextBox.Text = aQueue.Dequeue();
+                dComplainTextBox.Text = aQueue.Dequeue();
+               
+                listView1.Items.RemoveAt(0);
+
+
+            }
+            else
+            {
+                MessageBox.Show("You Have No Data In Queue");
+            }
+            
+        }
+    }
+}
