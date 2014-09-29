387ad2e470fcae147169bcc7cb76e194d35f0382
Badol Hossain <badolit009@gmail.com>
2014-09-29 00:10:32 -0700

    Circle App Added

---
+++ CircleApp/CircleApp/Circle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleApp
+{
+    class Circle
+    {
+        public double GetDiameter(double redias)
+        {
+            return 2*redias;
+        }
+
+        public double GetPerimeter(double redius)
+        {
+            return 2*Math.PI*redius;
+
+        }
+
+        public double GetArea(double redius)
+        {
+            return Math.PI*redius*redius;
+        }
+    }
+}
---
+++ CircleApp/CircleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double redius = 5;
+            Circle aCircle = new Circle();
+            double diameter = aCircle.GetDiameter(redius);
+            double perimeter = aCircle.GetPerimeter(redius);
+            double area = aCircle.GetArea(redius);
+
+            Console.WriteLine(diameter);
+            Console.WriteLine(perimeter);
+            Console.WriteLine(area);
+            Console.ReadKey();
+        }
+    }
+}
