2647bd569351e1f0e7369cf24b7806e4206183d8
Badol Hossain <badolit009@gmail.com>
2014-09-28 23:41:49 -0700

    UserDefineDesktopApp Added successfully

---
+++ UserDefineType/UserDefineType/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace UserDefineType
+{
+    public partial class Form1 : Form
+    {
+        employee aemployee = new employee();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //int id;
+        //string name;
+        //double salary;
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            aemployee.id =Convert.ToInt32( idTextBox.Text);
+            aemployee.name = nameTextBox.Text;
+            aemployee.salary = Convert.ToDouble(salaryTextBox.Text);
+
+            MessageBox.Show("Employee ID "+aemployee.id+"\nEmaployee Name "+aemployee.name+"\nEmployee Salary "+aemployee.salary);
+            idTextBox.Text = "";
+            nameTextBox.Text = "";
+            salaryTextBox.Text = "";
+        }
+
+        private void retrieveButton_Click(object sender, EventArgs e)
+        {
+            idTextBox.Text = aemployee.id.ToString();
+            nameTextBox.Text = aemployee.name;
+            salaryTextBox.Text = aemployee.salary.ToString();
+        }
+    }
+}
---
+++ UserDefineType/UserDefineType/employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UserDefineType
+{
+    class employee
+    {
+        public int id;
+        public string name;
+        public double salary;
+    }
+}
