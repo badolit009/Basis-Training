267aa7dd7634aa0b933bf054c7b11d6673567f73
Badol Hossain <badolit009@gmail.com>
2014-09-28 17:59:17 -0700

    Successfully added

---
+++ Employeesalaryexample/Employeesalaryexample/EmployeeSalary.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Employeesalaryexample
+{
+    partial class EmployeeSalary
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveTextBox = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.selaryTextBox = new System.Windows.Forms.TextBox();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // saveTextBox
+            // 
+            this.saveTextBox.Location = new System.Drawing.Point(317, 128);
+            this.saveTextBox.Name = "saveTextBox";
+            this.saveTextBox.Size = new System.Drawing.Size(101, 29);
+            this.saveTextBox.TabIndex = 0;
+            this.saveTextBox.Text = "Save";
+            this.saveTextBox.UseVisualStyleBackColor = true;
+            this.saveTextBox.Click += new System.EventHandler(this.saveTextBox_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID";
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Location = new System.Drawing.Point(163, 23);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(255, 20);
+            this.idTextBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Selary";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(36, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Address";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(163, 49);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(255, 20);
+            this.nameTextBox.TabIndex = 6;
+            // 
+            // selaryTextBox
+            // 
+            this.selaryTextBox.Location = new System.Drawing.Point(163, 76);
+            this.selaryTextBox.Name = "selaryTextBox";
+            this.selaryTextBox.Size = new System.Drawing.Size(255, 20);
+            this.selaryTextBox.TabIndex = 7;
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Location = new System.Drawing.Point(163, 102);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(255, 20);
+            this.addressTextBox.TabIndex = 8;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(39, 163);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(379, 151);
+            this.listView1.TabIndex = 9;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ID";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Name";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Selary";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Address";
+            // 
+            // EmployeeSalary
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 335);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.addressTextBox);
+            this.Controls.Add(this.selaryTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.idTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.saveTextBox);
+            this.Name = "EmployeeSalary";
+            this.Text = "EmployeeSalary";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button saveTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox selaryTextBox;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+    }
+}
+
---
+++ Employeesalaryexample/Employeesalaryexample/EmployeeSalary.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Employeesalaryexample
+{
+    public partial class EmployeeSalary : Form
+    {
+        public EmployeeSalary()
+        {
+            InitializeComponent();
+        }
+
+        List<int> idList= new List<int>();
+        List<string> namelist=new List<string>();
+        List<double> selaryList = new List<double>();
+        List<string>addressList=new List<string>();
+        private void saveTextBox_Click(object sender, EventArgs e)
+        {
+            int id=Convert.ToInt32(idTextBox.Text);
+            string name=nameTextBox.Text;
+            double selary=Convert.ToDouble(selaryTextBox.Text);
+            string address=addressTextBox.Text;
+
+            idList.Add(id);
+            namelist.Add(name);
+            selaryList.Add(selary);
+            addressList.Add(address);
+
+            idTextBox.Text = "";
+            nameTextBox.Text = "";
+            selaryTextBox.Text = "";
+            addressTextBox.Text = "";
+
+            ListViewItem items = new ListViewItem(id.ToString());
+            items.SubItems.Add(name);
+            items.SubItems.Add(selary.ToString());
+            items.SubItems.Add(address);
+            listView1.Items.Add(items);
+
+
+        }
+
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            int selected = listView1.SelectedIndices[0];
+            idTextBox.Text = idList[selected].ToString();
+            nameTextBox.Text=namelist[selected];
+            selaryTextBox.Text=selaryList[selected].ToString();
+            addressTextBox.Text=addressList[selected];
+        }
+    }
+}
