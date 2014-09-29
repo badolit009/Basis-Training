20d3538fa19ee7938e8d248ab2f9e48a4446e144
Badol Hossain <badolit009@gmail.com>
2014-09-29 00:09:07 -0700

    AccountTransactionApp Upload Successfully

---
+++ AccountTransactionApp/AccountTransactionApp/AccountOperationUI.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace AccountTransactionApp
+{
+    partial class AccountOperationUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.customerNameTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.createButton = new System.Windows.Forms.Button();
+            this.accountNumberTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.withdrawButton = new System.Windows.Forms.Button();
+            this.depositButton = new System.Windows.Forms.Button();
+            this.amountTextBox = new System.Windows.Forms.TextBox();
+            this.amount = new System.Windows.Forms.Label();
+            this.reportButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.customerNameTextBox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.createButton);
+            this.groupBox1.Controls.Add(this.accountNumberTextBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(477, 110);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Account Creation";
+            // 
+            // customerNameTextBox
+            // 
+            this.customerNameTextBox.Location = new System.Drawing.Point(147, 47);
+            this.customerNameTextBox.Name = "customerNameTextBox";
+            this.customerNameTextBox.Size = new System.Drawing.Size(306, 20);
+            this.customerNameTextBox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Customer Name";
+            // 
+            // createButton
+            // 
+            this.createButton.Location = new System.Drawing.Point(339, 73);
+            this.createButton.Name = "createButton";
+            this.createButton.Size = new System.Drawing.Size(114, 31);
+            this.createButton.TabIndex = 2;
+            this.createButton.Text = "Create";
+            this.createButton.UseVisualStyleBackColor = true;
+            this.createButton.Click += new System.EventHandler(this.createButton_Click);
+            // 
+            // accountNumberTextBox
+            // 
+            this.accountNumberTextBox.Location = new System.Drawing.Point(147, 13);
+            this.accountNumberTextBox.Name = "accountNumberTextBox";
+            this.accountNumberTextBox.Size = new System.Drawing.Size(306, 20);
+            this.accountNumberTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Account Number";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.withdrawButton);
+            this.groupBox2.Controls.Add(this.depositButton);
+            this.groupBox2.Controls.Add(this.amountTextBox);
+            this.groupBox2.Controls.Add(this.amount);
+            this.groupBox2.Location = new System.Drawing.Point(12, 128);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(477, 86);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Transaction";
+            // 
+            // withdrawButton
+            // 
+            this.withdrawButton.Location = new System.Drawing.Point(339, 48);
+            this.withdrawButton.Name = "withdrawButton";
+            this.withdrawButton.Size = new System.Drawing.Size(114, 31);
+            this.withdrawButton.TabIndex = 3;
+            this.withdrawButton.Text = "Withdraw";
+            this.withdrawButton.UseVisualStyleBackColor = true;
+            this.withdrawButton.Click += new System.EventHandler(this.withdrawButton_Click);
+            // 
+            // depositButton
+            // 
+            this.depositButton.Location = new System.Drawing.Point(219, 48);
+            this.depositButton.Name = "depositButton";
+            this.depositButton.Size = new System.Drawing.Size(114, 31);
+            this.depositButton.TabIndex = 2;
+            this.depositButton.Text = "Deposit";
+            this.depositButton.UseVisualStyleBackColor = true;
+            this.depositButton.Click += new System.EventHandler(this.depositButton_Click);
+            // 
+            // amountTextBox
+            // 
+            this.amountTextBox.Location = new System.Drawing.Point(147, 17);
+            this.amountTextBox.Name = "amountTextBox";
+            this.amountTextBox.Size = new System.Drawing.Size(306, 20);
+            this.amountTextBox.TabIndex = 1;
+            // 
+            // amount
+            // 
+            this.amount.AutoSize = true;
+            this.amount.Location = new System.Drawing.Point(26, 20);
+            this.amount.Name = "amount";
+            this.amount.Size = new System.Drawing.Size(43, 13);
+            this.amount.TabIndex = 0;
+            this.amount.Text = "Amount";
+            // 
+            // reportButton
+            // 
+            this.reportButton.Location = new System.Drawing.Point(231, 220);
+            this.reportButton.Name = "reportButton";
+            this.reportButton.Size = new System.Drawing.Size(234, 31);
+            this.reportButton.TabIndex = 4;
+            this.reportButton.Text = "Report";
+            this.reportButton.UseVisualStyleBackColor = true;
+            this.reportButton.Click += new System.EventHandler(this.reportButton_Click);
+            // 
+            // AccountOperationUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 261);
+            this.Controls.Add(this.reportButton);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "AccountOperationUI";
+            this.Text = "AccountOperationUI";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox customerNameTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button createButton;
+        private System.Windows.Forms.TextBox accountNumberTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button withdrawButton;
+        private System.Windows.Forms.Button depositButton;
+        private System.Windows.Forms.TextBox amountTextBox;
+        private System.Windows.Forms.Label amount;
+        private System.Windows.Forms.Button reportButton;
+    }
+}
+
---
+++ AccountTransactionApp/AccountTransactionApp/AccountOperationUI.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountTransactionApp
+{
+    public partial class AccountOperationUI : Form
+    {
+        private Account anAccount;
+
+        public AccountOperationUI()
+        {
+            InitializeComponent();
+        }
+
+        private void createButton_Click(object sender, EventArgs e)
+        {
+            if (accountNumberTextBox.Text != string.Empty && customerNameTextBox.Text != string.Empty)
+            {
+
+                anAccount = new Account();
+                anAccount.number = accountNumberTextBox.Text;
+                anAccount.name = customerNameTextBox.Text;
+                MessageBox.Show(@"Account Has Been Created");
+            }
+            else
+            {
+                MessageBox.Show(@"Plz Enter All Data");  
+            }
+
+        }
+
+        private void depositButton_Click(object sender, EventArgs e)
+        {
+            if (anAccount != null)
+            {
+                double amount = Convert.ToDouble(amountTextBox.Text);
+                anAccount.Deposit(amount);
+                MessageBox.Show(@"Amount has been deposited");
+            }
+            else
+            {
+                MessageBox.Show(@"Plz Create Your Account First Now");
+            }
+            amountTextBox.Text = string.Empty;
+
+        }
+
+        private void withdrawButton_Click(object sender, EventArgs e)
+        {
+            if (anAccount != null)
+            {
+                double amount = Convert.ToDouble(amountTextBox.Text);
+                anAccount.Withdraw(amount);
+                MessageBox.Show(@"Amount has been withdraw");
+            }
+            else
+            {
+                MessageBox.Show(@"Plz Create Your Account First Now");
+            }
+            amountTextBox.Text = string.Empty;
+            
+        }
+
+        private void reportButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(anAccount.name+", Your Account Number Is :"+anAccount.number+" and Its Balance Is : "+anAccount.balance);
+        }
+
+    }
+}
