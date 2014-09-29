7b1218ddbad3a751920026546502e8b5e7797c56
Badol Hossain <badolit009@gmail.com>
2014-09-28 23:28:20 -0700

    Maximum and minimum Number Added Successfully

---
+++ MaximumAndMinimumNumber/MaximumAndMinimumNumber/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace MaximumAndMinimumNumber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.maxNumberTexBox = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.firstNumberTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.secondNumberTextBox = new System.Windows.Forms.TextBox();
+            this.thirNumberTextBox = new System.Windows.Forms.TextBox();
+            this.fourthNumberTextBox = new System.Windows.Forms.TextBox();
+            this.fifthNumberTextBox = new System.Windows.Forms.TextBox();
+            this.minNumberTextBox = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1st";
+            // 
+            // maxNumberTexBox
+            // 
+            this.maxNumberTexBox.Location = new System.Drawing.Point(12, 262);
+            this.maxNumberTexBox.Name = "maxNumberTexBox";
+            this.maxNumberTexBox.Size = new System.Drawing.Size(88, 30);
+            this.maxNumberTexBox.TabIndex = 1;
+            this.maxNumberTexBox.Text = "Maximum";
+            this.maxNumberTexBox.UseVisualStyleBackColor = true;
+            this.maxNumberTexBox.Click += new System.EventHandler(this.maxNumberTexBox_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.fifthNumberTextBox);
+            this.groupBox1.Controls.Add(this.fourthNumberTextBox);
+            this.groupBox1.Controls.Add(this.thirNumberTextBox);
+            this.groupBox1.Controls.Add(this.secondNumberTextBox);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.firstNumberTextBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(315, 195);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // firstNumberTextBox
+            // 
+            this.firstNumberTextBox.Location = new System.Drawing.Point(111, 25);
+            this.firstNumberTextBox.Name = "firstNumberTextBox";
+            this.firstNumberTextBox.Size = new System.Drawing.Size(157, 20);
+            this.firstNumberTextBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "2nd";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "3rd";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "4th";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(29, 141);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(22, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "5th";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 217);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 240);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "label7";
+            // 
+            // secondNumberTextBox
+            // 
+            this.secondNumberTextBox.Location = new System.Drawing.Point(111, 55);
+            this.secondNumberTextBox.Name = "secondNumberTextBox";
+            this.secondNumberTextBox.Size = new System.Drawing.Size(157, 20);
+            this.secondNumberTextBox.TabIndex = 6;
+            // 
+            // thirNumberTextBox
+            // 
+            this.thirNumberTextBox.Location = new System.Drawing.Point(111, 83);
+            this.thirNumberTextBox.Name = "thirNumberTextBox";
+            this.thirNumberTextBox.Size = new System.Drawing.Size(157, 20);
+            this.thirNumberTextBox.TabIndex = 7;
+            // 
+            // fourthNumberTextBox
+            // 
+            this.fourthNumberTextBox.Location = new System.Drawing.Point(111, 111);
+            this.fourthNumberTextBox.Name = "fourthNumberTextBox";
+            this.fourthNumberTextBox.Size = new System.Drawing.Size(157, 20);
+            this.fourthNumberTextBox.TabIndex = 8;
+            // 
+            // fifthNumberTextBox
+            // 
+            this.fifthNumberTextBox.Location = new System.Drawing.Point(111, 138);
+            this.fifthNumberTextBox.Name = "fifthNumberTextBox";
+            this.fifthNumberTextBox.Size = new System.Drawing.Size(157, 20);
+            this.fifthNumberTextBox.TabIndex = 9;
+            // 
+            // minNumberTextBox
+            // 
+            this.minNumberTextBox.Location = new System.Drawing.Point(106, 262);
+            this.minNumberTextBox.Name = "minNumberTextBox";
+            this.minNumberTextBox.Size = new System.Drawing.Size(88, 30);
+            this.minNumberTextBox.TabIndex = 8;
+            this.minNumberTextBox.Text = "Minimum";
+            this.minNumberTextBox.UseVisualStyleBackColor = true;
+            this.minNumberTextBox.Click += new System.EventHandler(this.minNumberTextBox_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 296);
+            this.Controls.Add(this.minNumberTextBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.maxNumberTexBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button maxNumberTexBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox fifthNumberTextBox;
+        private System.Windows.Forms.TextBox fourthNumberTextBox;
+        private System.Windows.Forms.TextBox thirNumberTextBox;
+        private System.Windows.Forms.TextBox secondNumberTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox firstNumberTextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button minNumberTextBox;
+    }
+}
+
---
+++ MaximumAndMinimumNumber/MaximumAndMinimumNumber/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MaximumAndMinimumNumber
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<int> number = new List<int>();
+
+        private void maxNumberTexBox_Click(object sender, EventArgs e)
+        {
+            int firstNumber = Convert.ToInt32(firstNumberTextBox.Text);
+            int secondNumber = Convert.ToInt32(secondNumberTextBox.Text);
+            int thirdNumber = Convert.ToInt32(thirNumberTextBox.Text);
+            int fourthNumber = Convert.ToInt32(fourthNumberTextBox.Text);
+            int fifthNumber = Convert.ToInt32(fifthNumberTextBox.Text);
+
+            number.Add(firstNumber);
+            number.Add(secondNumber);
+            number.Add(thirdNumber);
+            number.Add(fourthNumber);
+            number.Add(fifthNumber);
+            number.Sort();
+
+            firstNumberTextBox.Text = "";
+            secondNumberTextBox.Text = "";
+            thirNumberTextBox.Text = "";
+            fourthNumberTextBox.Text = "";
+            fifthNumberTextBox.Text = "";
+
+            
+
+            label6.Text= "Maximum Number Is : "+number[4].ToString();
+            label7.Text = "";
+
+        }
+
+        private void minNumberTextBox_Click(object sender, EventArgs e)
+        {
+            label7.Text="Minimum Number Is : "+number[0].ToString();
+            label6.Text = "";
+        }
+    }
+}
