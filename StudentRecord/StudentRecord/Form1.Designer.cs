50754fa6f019172ba98d01e367a7ba13d13b0c96
Badol Hossain <badolit009@gmail.com>
2014-09-28 23:37:06 -0700

    StudentRecord Added Successfully

---
+++ StudentRecord/StudentRecord/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace StudentRecord
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.showButton = new System.Windows.Forms.Button();
+            this.registrationNoTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Registration No";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(294, 91);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(125, 26);
+            this.saveButton.TabIndex = 2;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(295, 123);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(125, 23);
+            this.showButton.TabIndex = 3;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            // 
+            // registrationNoTextBox
+            // 
+            this.registrationNoTextBox.Location = new System.Drawing.Point(197, 24);
+            this.registrationNoTextBox.Name = "registrationNoTextBox";
+            this.registrationNoTextBox.Size = new System.Drawing.Size(223, 20);
+            this.registrationNoTextBox.TabIndex = 4;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(197, 55);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(222, 20);
+            this.nameTextBox.TabIndex = 5;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(26, 157);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(393, 147);
+            this.listBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 323);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.registrationNoTextBox);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.TextBox registrationNoTextBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ StudentRecord/StudentRecord/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using CSVLib;
+
+namespace StudentRecord
+{
+    public partial class Form1 : Form
+    {
+        public string fileLocation = @"StudentRecord";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            FileStream aStreamForRead = new FileStream(fileLocation,FileMode.Open);
+            CsvFileReader aReader = new CsvFileReader(aStreamForRead) ;
+            List<string> aRecord = new List<string>();
+
+            while (aReader.ReadRow(aRecord))
+            {
+                String regiNo = aRecord[0];
+                if (registrationNoTextBox.Text == regiNo[0].ToString())
+                {
+                    MessageBox.Show("Registration Number Doesn't Exist");
+                    aStreamForRead.Close();
+                        return;
+                }
+                
+                
+            }
+
+
+            FileStream aFileStream = new FileStream(fileLocation ,FileMode.Append);
+            CsvFileWriter aWriter = new CsvFileWriter((aFileStream));
+            List<string> studentRecord = new List<string>();
+            studentRecord.Add(registrationNoTextBox.Text);
+            studentRecord.Add(nameTextBox.Text);
+            aWriter.WriteRow(studentRecord);
+            aFileStream.Close();
+
+            
+        }
+    }
+}
