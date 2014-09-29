f8cfe9b310451ede6fc946fca47d898157173571
Badol Hossain <badolit009@gmail.com>
2014-09-28 23:58:56 -0700

    StudentInformation Added Successfully

---
+++ StudentInformation/StudentInformation/StudentInfo.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace StudentInformation
+{
+    partial class StudentInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.regiNoTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.showButton = new System.Windows.Forms.Button();
+            this.retrieveButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lastNameTextBox);
+            this.groupBox1.Controls.Add(this.firstNameTextBox);
+            this.groupBox1.Controls.Add(this.regiNoTextBox);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(31, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(285, 152);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Student Information";
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(76, 95);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(201, 20);
+            this.lastNameTextBox.TabIndex = 5;
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Location = new System.Drawing.Point(76, 60);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(201, 20);
+            this.firstNameTextBox.TabIndex = 4;
+            // 
+            // regiNoTextBox
+            // 
+            this.regiNoTextBox.Location = new System.Drawing.Point(76, 25);
+            this.regiNoTextBox.Name = "regiNoTextBox";
+            this.regiNoTextBox.Size = new System.Drawing.Size(201, 20);
+            this.regiNoTextBox.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Last Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "First Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Regi No";
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(128, 200);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(77, 26);
+            this.showButton.TabIndex = 1;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // retrieveButton
+            // 
+            this.retrieveButton.Location = new System.Drawing.Point(214, 200);
+            this.retrieveButton.Name = "retrieveButton";
+            this.retrieveButton.Size = new System.Drawing.Size(77, 26);
+            this.retrieveButton.TabIndex = 2;
+            this.retrieveButton.Text = "Retrieve";
+            this.retrieveButton.UseVisualStyleBackColor = true;
+            this.retrieveButton.Click += new System.EventHandler(this.retrieveButton_Click);
+            // 
+            // StudentInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 261);
+            this.Controls.Add(this.retrieveButton);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "StudentInfo";
+            this.Text = "StudentInfo";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.TextBox regiNoTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.Button retrieveButton;
+    }
+}
+
---
+++ StudentInformation/StudentInformation/StudentInfo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentInformation
+{
+    public partial class StudentInfo : Form
+    {
+        private Student aStudent = new Student();
+        public StudentInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            aStudent.id = regiNoTextBox.Text;
+            aStudent.firstName = firstNameTextBox.Text;
+            aStudent.lastName = lastNameTextBox.Text;
+            regiNoTextBox.Text = string.Empty;
+            firstNameTextBox.Text=string.Empty;
+            lastNameTextBox.Text = string.Empty;
+            MessageBox.Show(aStudent.firstName+""+aStudent.lastName+"Your Registration No Is : "+aStudent.id);
+
+
+        }
+
+        private void retrieveButton_Click(object sender, EventArgs e)
+        {
+            regiNoTextBox.Text = aStudent.id;
+            firstNameTextBox.Text = aStudent.firstName;
+            lastNameTextBox.Text = aStudent.lastName;
+        }
+    }
+}
