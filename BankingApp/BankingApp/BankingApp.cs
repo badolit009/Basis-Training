d5194edeabdfc12b4e105eeb388447fcd3f38a51
Badol Hossain <badolit009@gmail.com>
2014-09-29 00:19:57 -0700

    BankingApp OOP use get set Upload successfully

---
+++ BankingApp/BankingApp/BankingApp.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BankingApp
+{
+    public partial class BankingApp : Form
+    {
+  
+        public BankingApp()
+        {
+            InitializeComponent();
+        }
+
+        Account aAccount = new Account();
+
+        private void createButton_Click(object sender, EventArgs e)
+        {
+            if (numberTextBox.Text != string.Empty && nameTextBox.Text != string.Empty)
+            {
+                aAccount.Number = numberTextBox.Text;
+                aAccount.Name = nameTextBox.Text;
+
+
+            }
+            else
+            {
+                MessageBox.Show("Plz Enter All Data");
+            }
+            numberTextBox.Text = string.Empty;
+            nameTextBox.Text = string.Empty;
+        }
+        private void depositButton_Click(object sender, EventArgs e)
+        {
+            if (amountTextBox.Text != string.Empty)
+            {
+                double amount = Convert.ToDouble(amountTextBox.Text);
+                aAccount.Deposit(amount);
+  
+            }
+            else
+            {
+                MessageBox.Show("Plz Enter Amount");
+            }
+            amountTextBox.Text = string.Empty;
+        }
+
+        private void withdrawButton_Click(object sender, EventArgs e)
+        {
+            if (amountTextBox.Text != string.Empty)
+            {
+                double amount = Convert.ToDouble(amountTextBox.Text);
+                aAccount.Withdraw(amount);
+               
+            }
+            else
+            {
+                MessageBox.Show("Plz Enter Amount");
+            }
+            amountTextBox.Text = string.Empty;
+        }
+        private void reportButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show( aAccount.Name + " , Your Account Number : " + aAccount.Number + "it's balance " + aAccount.Balance);
+
+        }
+
+
+
+    }
+}
---
+++ BankingAppOOP/BankingApp/Account.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingApp
+{
+    class Account
+    {
+        private string number;
+        private string name;
+        private double balance;
+
+        public string Number
+        {
+            set
+            {
+                number = value;
+            }
+            get
+            {
+                return number;
+            }
+        }
+
+        public string Name
+        {
+            set
+            {
+                name = value;
+            }
+            get
+            {
+                return name;
+            }
+        }
+
+        public double Balance
+        {
+            get
+            {
+                return balance;
+            }
+        }
+
+        public void Deposit(double amount)
+        {
+               balance +=amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            balance -= amount;
+        }
+
+        //public string GetReport()
+        //{
+        //    return Name + " , Your Account Number : " + Number + "it's balance " + Balance;
+
+        //}
+
+
+
+    }
+}
