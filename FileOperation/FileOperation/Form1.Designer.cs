8a8072a3e8a02d2680eab6b9e689a6968920be1e
Badol Hossain <badolit009@gmail.com>
2014-09-28 18:06:39 -0700

    Initialize FileOperation Uploaded

---
+++ FileOperation/FileOperation/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace FileOperation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveButton = new System.Windows.Forms.Button();
+            this.showButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.enterNameTextBox = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(346, 64);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(77, 31);
+            this.saveButton.TabIndex = 0;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(346, 101);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(77, 31);
+            this.showButton.TabIndex = 1;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter Name";
+            // 
+            // enterNameTextBox
+            // 
+            this.enterNameTextBox.Location = new System.Drawing.Point(151, 38);
+            this.enterNameTextBox.Name = "enterNameTextBox";
+            this.enterNameTextBox.Size = new System.Drawing.Size(272, 20);
+            this.enterNameTextBox.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(151, 144);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(271, 160);
+            this.listBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 372);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.enterNameTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.saveButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox enterNameTextBox;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ FileOperation/FileOperation/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FileOperation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string fileLocation = @"firstfile.txt";
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            FileStream afileStream = new FileStream(fileLocation,FileMode.Append);
+            StreamWriter aWriter = new StreamWriter(afileStream);
+            aWriter.WriteLine(enterNameTextBox.Text);
+            aWriter.Close();
+            enterNameTextBox.Text = "";
+
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            FileStream afileStream = new FileStream(fileLocation,FileMode.Open);
+            StreamReader aReader = new StreamReader(afileStream);
+            listBox1.Items.Clear();
+            while(!aReader.EndOfStream)
+            {
+                string aLine = aReader.ReadLine();
+                listBox1.Items.Add(aLine);
+            }
+            aReader.Close();
+        }
+    }
+}
