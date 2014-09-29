7ff107c462de01e14b4e6f4a918ad9ee67dc5c3d
Badol Hossain <badolit009@gmail.com>
2014-09-28 18:27:24 -0700

    loopDetailsPractice Uploaded Successfully

---
+++ loopDetailsPractice/loopDetailsPractice/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace loopDetailsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int i = 0;
+            //while (i<=999)
+            //{
+            //    Console.WriteLine(++i);
+            //}
+
+            int[] array = new int[400];
+            int index = 0;
+            for (int i = 100; i < 500; i++)
+            {
+                array[index] = i;
+                index++;
+            }
+
+            Console.WriteLine(array[399]);
+            Console.ReadKey();
+
+        }
+    }
+}
