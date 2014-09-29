5b9276de8f7077aa3cbcd062aba35d99f9ead649
Badol Hossain <badolit009@gmail.com>
2014-09-28 23:38:39 -0700

    Successfully Added

---
+++ UserDefineConsoleApp/UserDefineConsoleApp/Number.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UserDefineConsoleApp
+{
+    class Number
+    {
+        public int value;
+    }
+}
---
+++ UserDefineConsoleApp/UserDefineConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UserDefineConsoleApp
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<Number> numbers = new List<Number>();
+
+            
+            for (int i = 0; i < 10; i++)
+            {
+                Number aNumber = new Number();
+                aNumber.value = i;
+                numbers.Add(aNumber);
+            }
+            foreach (Number  item in numbers)
+            {
+                Console.WriteLine(item.value);
+            }
+            Console.ReadKey();
+        }
+    }
+}
